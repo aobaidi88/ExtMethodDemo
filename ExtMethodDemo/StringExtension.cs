acc0e18e5ffd53095940ba6f82db243b579b1d1d
ALI OBAIDI <aobaidi@cnm.edu>
2020-10-19 21:48:31 -0600

    Add project files.

---
+++ ExtMethodDemo/StringExtension.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtMethodDemo
+{
+    public static class StringExtension
+    {
+        public static string Capitalize(this String str)
+        {
+            if (str == null)
+            {
+                return null;
+            }
+            else if (str.Length > 1)
+            {
+                return char.ToUpper(str[0]) + str.Substring(1);
+            }
+            else
+            {
+                return str.ToUpper();
+            }
+        }
+    }
+
+}
+
